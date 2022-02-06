a145a80673f5ded1aee0bd6643495876f93fab2d
soloma70 <soloma70@gmail.com>
2022-02-06 17:13:33 +0200

    Modify like_posts_and_follower

--- app/locators.py
+++ app/locators.py
@@ -27,7 +27,7 @@ class TagsInsta:
 class PostInsta:
     user_name = (By.CSS_SELECTOR, 'span > a')
     wrong_userpage = (By.CSS_SELECTOR, '#react-root > section > main > div > div > h2')
-
+    wrong_userpage1 = (By.CSS_SELECTOR, 'body > div > div > div > div > h2')
 
 
 class UserInsta:
@@ -36,17 +36,21 @@ class UserInsta:
         By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[2]/div/div/div/span/span[1]/button')
     user_send_message = (By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[2]/div/div/button/div')
     user_request_subscribe = (By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[2]/div/div/button')
-    '//*[@id="react-root"]/section/main/div/header/section/div[2]/div/div/button'
-    '//*[@id="react-root"]/section/main/div/header/section/div[2]/div/div/button'
+
+    user_unsubscribe_confirm = (By.XPATH, '/html/body/div[6]/div/div/div/div[3]/button[1]')
+    user_unsubscribe_cancel = (By.XPATH, '/html/body/div[6]/div/div/div/div[3]/button[2]')
 
     user_posts = (By.XPATH, '//*[@id="react-root"]/section/main/div/ul/li[1]/span/span')
     user_follow = (By.CSS_SELECTOR, 'ul > li > a > span')  # 2
     followers = (By.CSS_SELECTOR, 'li > div > div > div > div > span > a')
-    followers_ul = (By.CSS_SELECTOR, 'body > div.RnEpo.Yx5HN > div > div > div.isgrP')
+    followers_ul = (By.CSS_SELECTOR, 'body > div.RnEpo.Yx5HN > div > div > div > div.isgrP')
+    followers_close = (
+    By.CSS_SELECTOR, 'body > div.RnEpo.Yx5HN > div > div > div > div:nth-child(1) > div > div:nth-child(3) > button')
+    followings_ul = (By.CSS_SELECTOR, 'body > div.RnEpo.Yx5HN > div > div > div > div.isgrP')
 
     user_post_like = (By.CSS_SELECTOR, 'button[type="button"] > div > span > svg')
     user_post_unlike = (By.XPATH,
-    '//*[@id="react-root"]/section/main/div/div[1]/article/div/div[3]/div/div/section[1]/span[1]/button/div/span/svg')
+                        '//*[@id="react-root"]/section/main/div/div[1]/article/div/div[3]/div/div/section[1]/span[1]/button/div/span/svg')
 
 
 class MyProfile:
--- app/main.py
+++ app/main.py
@@ -35,6 +35,7 @@ class InstaBot():
         self.driver.quit()
 
     def selector_exist(self, selector: tuple) -> bool:
+        """метод проверяет по переданому селектору существует ли элемент на странице"""
 
         try:
             self.driver.find_element(*selector)
@@ -46,6 +47,7 @@ class InstaBot():
         return exist
 
     def auth(self):
+        """Метод авторизации на сайте Инстаграм"""
 
         try:
             self.driver.get('https://www.instagram.com/')
@@ -74,14 +76,17 @@ class InstaBot():
             self.close_browser()
 
     def goto_profile(self, user: str):
+        """Метод перехода на профиль пользователя"""
 
         self.driver.get(f'https://www.instagram.com/{user}/')
 
     def goto_tag(self, tag: str):
-
+        """Метод перехода по тегу"""
         self.driver.get(f'https://www.instagram.com/explore/tags/{tag}/')
 
     def parce_posts_by_tag(self, tag: str):
+        """Метод создания списка постов по тегу.
+        Принимает тег, парсит 24+12*10 первых постов, записывает в файл."""
 
         self.driver.get(f'https://www.instagram.com/explore/tags/{tag}/')
         sleep(randrange(3, 5))
@@ -113,6 +118,9 @@ class InstaBot():
                 text_file.write(link + '\n')
 
     def parce_users_post_by_tag(self, tag: str):
+        """Метод создания списка уникальных пользователей из постов по тегу.
+        Принимает тег, читает файл постов, парсит пользоваьелей, создает и записывает в файл
+        список уникальных пользователей."""
 
         with open(f'files\\{tag}_links.txt') as file:
             urls_list = file.readlines()
@@ -150,17 +158,17 @@ class InstaBot():
             for user in users_url:
                 text_file.write(user + '\n')
 
-    def parse_follower_users(self, user_url: str):
+    def parse_followers_users(self, user_name: str):
+        """Метод создания списка уникальных подписчиков пользователя.
+        Принимает имя пользователя, читает подписчиков, создает и записывает в файл список подписчиков."""
 
         try:
-            self.driver.get(user_url)
+            self.driver.get(f'https://www.instagram.com/{user_name}/')
             sleep(randrange(4, 10))
-
-            user_name = user_url.split('/')[-2]
 
             # создаём папку с именем пользователя
             if os.path.exists(f'files\\{user_name}'):
-                print(f"Папка c пользователем {user_name} уже существует!")
+                print(f"Папка c пользователем {user_name} уже существует.")
             else:
                 print(f"Создаём папку пользователя {user_name}.")
                 os.mkdir(f'files\\{user_name}')
@@ -170,7 +178,7 @@ class InstaBot():
                 user_follower = int(''.join(user_follower))
                 loops_count = int(user_follower / 12)
 
-                print(f'Пользователь {user_name}, подписчиков {user_follower}, итераций {loops_count}.')
+                print(f'Пользователь {user_name}: {user_follower} подписчиков, {loops_count} итераций.')
                 sleep(randrange(2, 4))
 
                 self.driver.find_elements(*UserInsta.user_follow)[0].click()
@@ -181,16 +189,12 @@ class InstaBot():
                 try:
                     for i in range(1, loops_count + 1):
                         self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", followers_ul)
-                        print(f'\rИтерация # {i}. ', end='')
+                        print(f'\r   Итерация # {i}. ', end='')
                         sleep(randrange(4, 7))
                         if i % 15 == 0:
-                            rand = randrange(40, 60)
-                            print(f'Засыпаем на {rand} сек.')
-                            sleep(rand)
+                            self.delay_action(30)
                         if i % 30 == 0:
-                            rand = randrange(40, 60)
-                            print(f'Засыпаем еще на {rand} сек.')
-                            sleep(rand)
+                            self.delay_action(40)
 
                 except Exception as ex:
                     print(f'Вызвано исключение: {ex}')
@@ -204,11 +208,80 @@ class InstaBot():
 
                     print(
                         f'Сохраняем подписчиков пользователя {user_name} в файл "files\\{user_name}\\{user_name}_followers.txt"')
-                    with open(f"files\\{user_name}\\{user_name}_followers.txt", "a") as text_file:
+                    with open(f"files\\{user_name}\\{user_name}_followers.txt", "w") as text_file:
                         for link in followers_urls:
                             text_file.write(link + "\n")
 
                     print(f'Обработка подписчиков пользователя {user_name} завершена')
+
+                    self.driver.find_element(*UserInsta.followers_close).click()
+
+            else:
+                print(f'Пользователь {user_name} не открывается')
+        except Exception as ex:
+            print(f'Вызвано исключение: {ex}')
+            self.close_browser()
+
+    def parse_followings_users(self, user_name: str):
+        """Метод создания списка уникальных подписок пользователя.
+        Принимает имя пользователя, читает подписки, создает и записывает в файл список подписок."""
+
+        try:
+            self.driver.get(f'https://www.instagram.com/{user_name}/')
+            sleep(randrange(4, 10))
+
+            # создаём папку с именем пользователя
+            if os.path.exists(f'files\\{user_name}'):
+                print(f"Папка c пользователем {user_name} уже существует.")
+            else:
+                print(f"Создаём папку пользователя {user_name}.")
+                os.mkdir(f'files\\{user_name}')
+
+            if self.selector_exist(UserInsta.user_name):
+                user_following = self.driver.find_elements(*UserInsta.user_follow)[1].text.split()
+                user_following = int(''.join(user_following))
+                loops_count_following = int(user_following / 12)
+
+                # Чтение подписок
+                print(f'Пользователь {user_name}: {user_following} подписок, {loops_count_following} итераций.')
+                sleep(randrange(2, 4))
+
+                self.driver.find_elements(*UserInsta.user_follow)[1].click()
+                followings_ul = self.driver.find_element(*UserInsta.followings_ul)
+
+                followings_urls = []
+
+                try:
+                    for i in range(1, loops_count_following + 2):
+                        self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight",
+                                                   followings_ul)
+                        print(f'\r   Итерация # {i}. ', end='')
+                        sleep(randrange(4, 7))
+                        if i % 15 == 0:
+                            self.delay_action(30)
+                        if i % 30 == 0:
+                            self.delay_action(40)
+
+                except Exception as ex:
+                    print(f'Вызвано исключение: {ex}')
+
+                finally:
+                    print('Обрабатываем URL подписок')
+                    all_followings_urls = followings_ul.find_elements(By.TAG_NAME, 'li')
+                    for url in all_followings_urls:
+                        url = url.find_element(By.TAG_NAME, 'a').get_attribute("href")
+                        followings_urls.append(url)
+
+                    print(
+                        f'Сохраняем подписки пользователя {user_name} в файл "files\\{user_name}\\{user_name}_followings.txt"')
+                    with open(f"files\\{user_name}\\{user_name}_followings.txt", "w") as text_file:
+                        for link in followings_urls:
+                            text_file.write(link + "\n")
+
+                    print(f'Обработка подписок пользователя {user_name} завершена')
+
+                    self.driver.find_element(*UserInsta.followers_close).click()
+
             else:
                 print(f'Пользователь {user_name} не открывается')
         except Exception as ex:
@@ -231,6 +304,9 @@ class InstaBot():
         print('\r ', end='')
 
     def like_posts_and_follower(self, user: str):
+        """Метод подписки и лайков на посты подписчиков пользователя, полученного по посту тега.
+        Принимает имя пользователя, читает файл с подписчиками, переходит на страницу каждого подписчика,
+        подписывается и ставит лайки на первые 10 (или меньше) постов подписчика."""
 
         with open(f'files\\{user}\\{user}_followers.txt') as file:
             followers_url = file.readlines()
@@ -239,14 +315,15 @@ class InstaBot():
         print(f'Прочитано {len(followers_url)} ссылок на профили подписчиков пользователя {user}')
 
         subscribe_list = []
-        for i, follower_url in enumerate(followers_url[21:41]):
+        for i, follower_url in enumerate(followers_url[33:41]):
             try:
                 follower = follower_url.split('/')[-2]
                 self.driver.get(follower_url)
                 sleep(randrange(4, 6))
                 print(f'Итерация # {i + 1}. Пользователь {follower}... ', end='')
 
-                if not self.selector_exist(PostInsta.wrong_userpage):
+                if (not self.selector_exist(PostInsta.wrong_userpage1)) or (
+                        not self.selector_exist(PostInsta.wrong_userpage)):
                     self.driver.find_element(*MyProfile.edit_btn)
                     # Подписка на пользователя
                     if self.selector_exist(UserInsta.user_subscribe) and not self.selector_exist(
@@ -292,11 +369,6 @@ class InstaBot():
                             print(
                                 f'   Постов прочитано: {amount_parce_posts}. Ставим лайки на первые {amount_like_posts}.')
 
-                        # print(f'Сохранение в файл {tag}_links.txt')
-                        # with open(f'files\\{tag}_links.txt', 'a') as text_file:
-                        #     for link in posts_url:
-                        #         text_file.write(link + '\n')
-
                         print('\r ', end='')
 
                         for i, post_url in enumerate(posts_url[0:amount_like_posts]):
@@ -333,18 +405,97 @@ class InstaBot():
                 print(f'Вызвано исключение: {ex}')
                 self.close_browser()
 
+    def compare_followers_following_lists(self, user_name: str):
+        """Метод сравнения списков подписчиков и подписок, получения списка не подписаных на нас пользователей.
+        Принимает имя пользователя, читает подписчиков и подписки, создает и записывает в файл
+        неподписанных пользователей."""
+
+        print()
+        if os.path.isfile(f'files\\{user_name}\\{user_name}_followers.txt') and os.path.isfile(
+                f'files\\{user_name}\\{user_name}_followings.txt'):
+            with open(f'files\\{user_name}\\{user_name}_followers.txt') as file:
+                followers_url = file.readlines()
+            print(f'Прочитано {len(followers_url)} ссылок на профили подписчиков пользователя {user_name}')
+
+            with open(f'files\\{user_name}\\{user_name}_followings.txt') as file:
+                followings_url = file.readlines()
+            print(f'Прочитано {len(followings_url)} ссылок на профили подписок пользователя {user_name}')
+
+            count = 0
+            unfollow_list = []
+            for user in followings_url:
+                if user not in followers_url:
+                    count += 1
+                    unfollow_list.append(user)
+            print(f"Необходимо отписаться от {count} пользователей")
+
+            # сохраняем всех от кого нужно отписаться в файл
+            with open(f"files\\{user_name}\\{user_name}_unfollow_list.txt", "w") as unfollow_file:
+                for user in unfollow_list:
+                    unfollow_file.write(user)
+        else:
+            print(f'Отсутствует файл {user_name}_followers.txt или {user_name}_followings.txt')
+
+    def smart_unsubscribe(self, user_name: str):
+
+        print()
+        if os.path.isfile(f'files\\{user_name}\\{user_name}_unfollow_list.txt'):
+            with open(f'files\\{user_name}\\{user_name}_unfollow_list.txt') as file:
+                unfollowers_url = file.readlines()
+            print(f'Прочитано {len(unfollowers_url)} ссылок на профили для отписок.')
+
+        for i, unfollower_url in enumerate(unfollowers_url[5:25]):
+            try:
+                unfollower = unfollower_url.split('/')[-2]
+                self.driver.get(unfollower_url)
+                sleep(randrange(4, 6))
+                print(f'Итерация # {i + 1}. Пользователь {unfollower}... ', end='')
+
+                if (not self.selector_exist(PostInsta.wrong_userpage1)) or (
+                        not self.selector_exist(PostInsta.wrong_userpage)):
+                    self.driver.find_element(*MyProfile.edit_btn)
+                    # Отписка от пользователя
+                    if self.selector_exist(UserInsta.user_send_message):
+                        self.driver.find_element(*UserInsta.user_subscribe).click()
+                        self.driver.find_element(*UserInsta.user_unsubscribe_confirm).click()
+                        print('Успешно отписались!')
+
+                        self.delay_action(80)
+
+                    elif self.driver.find_element(*UserInsta.user_subscribe).text == 'Подписаться':
+                        print('Уже отписались!')
+
+                else:
+                    print(f'Не удалось прочитать пользователя {unfollower}')
+
+            except Exception as ex:
+                print(f'Вызвано исключение: {ex}')
+                self.close_browser()
 
 instabot = InstaBot(kardon_login, kardon_passw)
 try:
     instabot.auth()
     sleep(randrange(3, 5))
+    # Чтение ссылок на посты по тегу
     # instabot.parce_posts_by_tag('СовместныеПокупкиХарьков')
+    # Парсинг пользователей по постам в теге
     # instabot.parce_users_post_by_tag('СовместныеПокупкиХарьков')
-    # instabot.parse_follower_users('https://instagram.com/usa_shop_kharkov/')
+    # Парсинг подписчиков пользователя тега
+    # instabot.parse_follower_users('usa_shop_kharkov')
     # sleep(randrange(4, 6))
-    instabot.like_posts_and_follower('usa_shop_kharkov')
+    # Подписки и лайки на посты подписчиков пользователя
+    # instabot.like_posts_and_follower('shopping.usa.brand')
+    # Парсинг подписчиков пользователя
+    # instabot.parse_followers_users(kardon_login)
+    # Парсинг подписок пользователя
+    # instabot.parse_followings_users(kardon_login)
+    # Сравнение подписчиков и подписок и получение файла отписок
+    # instabot.compare_followers_following_lists(kardon_login)
+    #
+    instabot.smart_unsubscribe(kardon_login)
     instabot.goto_profile(kardon_login)
     sleep(randrange(3, 5))
+    #
     instabot.exit_profile()
     sleep(randrange(3, 5))
 except Exception as ex:
